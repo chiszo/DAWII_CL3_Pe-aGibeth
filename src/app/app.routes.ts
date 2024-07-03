16ddc51dce47ff435213988824f84f5a4d4fd11c
chiszo <gigibethandrea@gmail.com>
2024-07-03 14:08:28 -0500

    creacion de las páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Pregunta2Component } from './dashboard/pregunta2/pregunta2.component';
+import { Pregunta3Component } from './dashboard/pregunta3/pregunta3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"dashboard", component: DashboardComponent,
+    children: [
+      {path: "pregunta2", component: Pregunta2Component},
+      {path: "pregunta3", component: Pregunta3Component},
+    ]
+  },
+  {path: "", redirectTo: "dashboard", pathMatch:"full"}
+];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../angular-material/material/material.module';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  constructor(private router:Router, private route: ActivatedRoute){}
+
+  irPregunta2():void{
+    this.router.navigate(["pregunta2"],{relativeTo: this.route})
+  }
+  irPregunta3():void{
+    this.router.navigate(["pregunta3"],{relativeTo: this.route})
+  }
 }
